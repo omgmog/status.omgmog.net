d28af5171dcbd1c54c16ee9a27fb71068e56721e
Max Glenister <omgmog@users.noreply.github.com>
2019-04-02 19:21:55 +0100

    Create newpost-bookmarklet.js

---
+++ newpost-bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function(){var name=Math.floor(Date.now()/1000);window.open("https://github.com/omgmog/status.omgmog.net/new/master/_updates/"+name+"?filename="+name+".md&value=---%250A---%250A");}());
